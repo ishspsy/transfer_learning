229dfab6633cf31165b7a37055f68c79f77b6791
ishspsy <ishspsy@gmail.com>
2025-07-21 09:49:42 +0900

    Update Simulation_estimation_error(Figure_S11).R

--- Code_example/Simulation_estimation_error(Figure_S11).R
+++ Code_example/Simulation_estimation_error(Figure_S11).R
@@ -5,10 +5,15 @@ source(paste0(path,"/Function/Functions_rankestimation_simul.R")) # load r funct
 
 
 ########################### Hetergoeneous design setting ###########################################
+## generating target data
 tgarget_n050p30r5q20 <- generator_target_simulsourcedetection(rep=100, n=50, p=30, q=20, r= 5, corx=0.5,cory=0)
+
+# generating source datasets
 Sourceset_h5 <- generator_source_simulsourcedetection_het(rep=100, nvec=rep(60,8),hvec = c(5,5,5,20,20,20,30,30), 
                                                           p=30,q=20,ranksourcevec=rep(5,8),
                                                           B=tgarget_n050p30r5q20$B,cory=0,numsource=5)
+
+# conduct simulation analysis
 testrep1to3_het <- rep_forsimul(Ylist=tgarget_n050p30r5q20$Ylist,
                                 Xlist=tgarget_n050p30r5q20$Xlist,
                                 auxYlist_list=Sourceset_h5$auxYlist_list,
